9556edf6263f2302af638e6a71598533782a7552
alex-zander-0403 <alexzander0403@gmail.com>
2026-03-22 15:55:34 +0300

    add instructions for types in users.ts

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,32 +1,32 @@
-// названия action типов для стандартизации
+//Enum для стандартизации названий action типов действий
 export enum UserActionTypes {
   FETCH_USERS = "FETCH_USERS",
   FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS",
   FETCH_USERS_ERROR = "FETCH_USERS_ERROR",
 }
 
-// интерфейс для users state
+// интерфейс состояния user state
 export interface UserState {
-  users: any[];
-  loading: boolean;
-  error: null | string;
+  users: any[]; // массив пользователей
+  loading: boolean; // флаг загрузки
+  error: null | string; // ошибка или null
 }
 
 // ----
 
 // интерфейс для получения users
 interface FetchUsersAction {
-  type: UserActionTypes.FETCH_USERS;
+  type: UserActionTypes.FETCH_USERS; // конкретное значение из enum
 }
 // интерфейс для успешного получения users
 interface FetchUsersSuccessAction {
-  type: UserActionTypes.FETCH_USERS_SUCCESS;
-  payload: any[];
+  type: UserActionTypes.FETCH_USERS_SUCCESS; // конкретное значение из enum
+  payload: any[]; // массив пользователей
 }
 // интерфейс для ошибки получения users
 interface FetchUsersErrorAction {
-  type: UserActionTypes.FETCH_USERS_ERROR;
-  payload: string;
+  type: UserActionTypes.FETCH_USERS_ERROR; // конкретное значение из enum
+  payload: string; // текст ошибки
 }
 
 // union тип для action
